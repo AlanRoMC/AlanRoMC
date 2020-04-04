7b4a56ce73b977a89084be796fdc21cba12a6402
AlanRoMC <60676415+AlanRoMC@users.noreply.github.com>
2020-04-03 21:01:36 -0600

    Add files via upload

--- Apps/Reproduccion/models.py
+++ Apps/Reproduccion/models.py
@@ -14,6 +14,13 @@ class Cancion(models.Model):
     calificacion = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
     album = models.ForeignKey('Album', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return "{} - {}".format(self.nombre, self.album)
+
+    class Meta:
+        verbose_name="Cancion"
+        verbose_name_plural="Canciones"
+
 class Album(models.Model):
     nombre = models.CharField(max_length=30)
     duracion = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
@@ -22,9 +29,12 @@ class Album(models.Model):
     genero = models.ForeignKey('Genero', on_delete=models.CASCADE)
     disquera = models.ForeignKey('Disquera', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.nombre
+
 class Disquera(models.Model):
     nombre = models.CharField(max_length=60)
-    direccion = models.CharField(max_length=100, null=True)
+    direccion = models.CharField(max_length=100, null=True, blank=True)
 
 class Genero(models.Model):
     nombre = models.CharField(max_length=50, unique=True)
@@ -38,3 +48,15 @@ class Playlist(models.Model):
 class PlaylistCanciones(models.Model):
     playlist = models.ForeignKey('Playlist', on_delete=models.CASCADE)
     cancion = models.ForeignKey('Cancion', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name="PlaylistCanciones"
+        verbose_name_plural="PlaylistsCanciones"
+
+class UsuarioCanciones(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    cancion = models.ForeignKey('Cancion', on_delete=models.CASCADE)
+    
+    class Meta:
+        verbose_name="UsuarioCanciones"
+        verbose_name_plural="UsuariosCanciones"
